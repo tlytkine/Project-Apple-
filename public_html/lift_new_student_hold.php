b586b80f8202b2297e84de1574d6f052d181cf69
Timothy Lytkine <tlytkine@gmail.com>
2018-05-07 21:34:45 -0400

    lift new student hold page refresh added

--- public_html/lift_new_student_hold.php
+++ public_html/lift_new_student_hold.php
@@ -120,6 +120,7 @@
 
 		if($lift_hold_result && $delete_form_result){
 			echo "Hold was lifted successfully! New student advising form has been deleted from system.";
+			header("Location: lift_new_student_hold.php");
 		}
 		else{
 			echo "Hold was not able to be lifted.";
