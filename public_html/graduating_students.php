b1a9e5eb90935b0e7058f7a9dc114f928e877150
Timothy Lytkine <tlytkine@gmail.com>
2018-05-07 23:04:27 -0400

    advisor assignments

--- public_html/graduating_students.php
+++ public_html/graduating_students.php
@@ -22,9 +22,8 @@ echo "<h1>List of Graduating Students</h1>";
 
 // Checks if there are any graduating students present in the system.
 $graduating_check_query = "SELECT studentid FROM graduationapplication;";
-$graduating_check_result - mysqli_query($connection, $graduating_check_query);
+$graduating_check_result = mysqli_query($connection, $graduating_check_query);
 $row = mysqli_fetch_assoc($graduating_check_result);
-
 
 	if(ISSET($row['studentid'])){
 		$graduating_students_query = "SELECT DISTINCT personalinfo.firstname,personalinfo.lastname,graduationapplications.studentid, graduationapplications.year,advises.degreename FROM personalinfo,graduationapplication,advises WHERE graduationapplication.studentid = personalinfo.id AND advises.studentid = graduationapplication.studentid AND graduationapplication.cleared=1;";
@@ -49,6 +48,7 @@ $row = mysqli_fetch_assoc($graduating_check_result);
 		}
 		echo "</table>";
 	}
+
 	else {
 		echo "There are no currently no graduating students in the system.";
 	}
