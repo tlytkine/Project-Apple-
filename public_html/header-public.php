eb1cc3aec16276ee0ab129c944029b372f242839
dreidenbaugh <dreidenbaugh@gwmail.gwu.edu>
2018-05-08 09:22:04 -0400

    Improve style

---
+++ public_html/header-public.php
@@ -0,0 +1,7 @@
+<div id="head-public">
+<div id="logo">
+	<span class="imgspan"></span>
+	<img src="https://creativeservices.gwu.edu/sites/g/files/zaxdzs1101/f/image/gw_txt_4cp_pos_0.png" height="80%">
+</div>
+</div>
+<div id="content">
